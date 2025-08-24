10d0eb98987a9041eee931786b50eed8ca8921f5
Vidocq17 <avidanB1704@gmail.com>
2025-08-24 22:12:35 +0200

    images pour l'appli + fix router netlify

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
+import { fileURLToPath, URL } from 'node:url' // ← important pour ESM
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Daily Monitoring',
+        short_name: 'DailyMonitor',
+        description: 'Suivi quotidien de régime et activité',
+        theme_color: '#2563EB',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
