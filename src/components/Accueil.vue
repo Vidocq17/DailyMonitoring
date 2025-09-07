e2138279d1b9b6c7baaa403c2078c4f38e70b06f
Vidocq17 <avidanB1704@gmail.com>
2025-09-07 23:46:31 +0200

    refactor: Remove unused buttons and styles from Accueil component

--- src/components/Accueil.vue
+++ src/components/Accueil.vue
@@ -3,12 +3,6 @@
     <h1>Bienvenue sur Daily Monitoring</h1>
     <p>Objectif : 85 kilos !</p>
     <p v-if="lastWeight">Poids actuel : {{ lastWeight }} kg</p>
-
-    <div class="home-buttons">
-      <router-link to="/add" class="btn">Ajouter</router-link>
-      <router-link to="/stats" class="btn">Graphiques</router-link>
-      <router-link to="/history" class="btn">Historique</router-link>
-    </div>
   </div>
 </template>
 
@@ -45,28 +39,6 @@ const lastWeight = computed(() => store.getLastWeight())
   color: #555;
 }
 
-.home-buttons {
-  display: flex;
-  justify-content: center;
-  gap: 1rem;
-  flex-wrap: wrap;
-}
-
-.btn {
-  display: inline-block;
-  padding: 0.8rem 1.5rem;
-  background-color: #4a90e2;
-  color: white;
-  text-decoration: none;
-  border-radius: 8px;
-  transition: background-color 0.3s;
-  font-size: 1rem;
-}
-
-.btn:hover {
-  background-color: #357ab8;
-}
-
 /* Responsive */
 @media (max-width: 600px) {
   .home h1 {
@@ -75,11 +47,6 @@ const lastWeight = computed(() => store.getLastWeight())
 
   .home p {
     font-size: 1rem;
-  }
-
-  .btn {
-    width: 100%;
-    text-align: center;
   }
 }
 </style>
