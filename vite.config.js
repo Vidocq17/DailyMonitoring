9e17f41c4c22e162d0e6d338bfe200892030452d
Vidocq17 <avidanB1704@gmail.com>
2025-09-08 11:22:42 +0200

    feat: update PWA icon paths and add new logo image

--- vite.config.js
+++ vite.config.js
@@ -17,15 +17,15 @@ export default defineConfig({
         display: 'standalone',
         start_url: '/',
         icons: [
-          { src: '/pwa-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/pwa-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      }
-    })
+          { src: '/public/pwa-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/public/pwa-512.png', sizes: '512x512', type: 'image/png' },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
