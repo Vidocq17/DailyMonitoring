c76f3835023a6c862e0b278afd22b3b8a0d7bf3f
Vidocq17 <avidanB1704@gmail.com>
2025-09-11 14:14:25 +0200

    feat: add moon and sun SVG icons for dark mode toggle

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
-
+import Moon from '@/assets/svg/moon.svg'
+import Sun from '@/assets/svg/sun.svg'
 const isDark = ref(false)
 
 const readTheme = () => document.documentElement.getAttribute('data-theme') === 'dark'
@@ -19,12 +20,8 @@ onMounted(() => {
 
 <template>
   <div>
-    <button
-      type="button"
-      @click="toggle"
-      class="flex justify-center items-center gap-2 !m-0 !w-full"
-    >
-      {{ isDark ? 'Clair' : 'Sombre' }}
+    <button type="button" @click="toggle" class="!p-2 !m-0 !w-full">
+      <img :src="isDark ? Sun : Moon" class="w-7 h-7" />
     </button>
   </div>
 </template>
