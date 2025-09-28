ef165e929e394047c8eff0799ea734deb6eca096
Vidocq17 <AvidanB1704@gmail.com>
2025-09-28 20:14:32 +0200

    Implement dark mode toggle functionality in DarkModeButton component

--- src/components/DarkModeButton.vue
+++ src/components/DarkModeButton.vue
@@ -1,3 +1,33 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import Moon from '@/assets/svg/moon.svg'
+import Sun from '@/assets/svg/sun.svg'
+const isDark = ref(false)
+
+const readTheme = () => document.documentElement.getAttribute('data-theme') === 'dark'
+const applyTheme = (dark: boolean) => {
+  document.documentElement.setAttribute('data-theme', dark ? 'dark' : 'light')
+  localStorage.setItem('theme', dark ? 'dark' : 'light')
+  isDark.value = dark
+}
+
+const toggle = () => applyTheme(!isDark.value)
+
+onMounted(() => {
+  isDark.value = readTheme()
+})
+</script>
+
+<template>
+  <div>
+    <button type="button" @click="toggle" class="!p-2 !m-0 !w-full">
+      <img :src="isDark ? Sun : Moon" class="w-7 h-7" />
+    </button>
+  </div>
+</template>
+
+<!-- 
+
 <script setup>
 import { onMounted, watch } from 'vue'
 import { storeToRefs } from 'pinia'
@@ -26,4 +56,4 @@ watch(isDark, (newVal) => {
 const toggle = () => {
   isDark.value = !isDark.value
 }
-</script>
+</script> -->
